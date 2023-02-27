8dc313dea36fdbf822e508d4082ce85c5b8320ca
Quojo_Steve <quojosteve@gmail.com>
2023-02-27 00:25:50 +0000

    finally done

---
+++ app/src/main/java/com/example/redo/OnFetchDataListener.java
@@ -0,0 +1,10 @@
+package com.example.redo;
+
+import com.example.redo.Models.NewsHeadlines;
+
+import java.util.List;
+
+public interface OnFetchDataListener<NewsApiResponse> {
+    void onFetchData(List<NewsHeadlines> list, String message);
+    void onError(String message);
+}
---
+++ app/src/main/java/com/example/redo/SelectListener.java
@@ -0,0 +1,7 @@
+package com.example.redo;
+
+import com.example.redo.Models.NewsHeadlines;
+
+public interface SelectListener {
+    void OnNewsClicked(NewsHeadlines headlines);
+}
